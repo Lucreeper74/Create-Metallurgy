9048319dcf07457ddd618094ae2c5a10bc74b70e
Luc_Creeper74 <lucreeper74@gmail.com>
2023-09-04 18:15:40 +0200

    Fix goggles info

--- src/main/java/fr/lucreeper74/createmetallurgy/content/castingbasin/CastingBasinBlockEntity.java
+++ src/main/java/fr/lucreeper74/createmetallurgy/content/castingbasin/CastingBasinBlockEntity.java
@@ -2,9 +2,20 @@ package fr.lucreeper74.createmetallurgy.content.castingbasin;
 
 import com.simibubi.create.content.processing.basin.BasinBlockEntity;
 import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
+import com.simibubi.create.foundation.utility.Components;
+import com.simibubi.create.foundation.utility.LangBuilder;
+import fr.lucreeper74.createmetallurgy.utils.LANG;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.templates.FluidTank;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemStackHandler;
 
 import java.util.List;
 
@@ -16,5 +27,49 @@ public class CastingBasinBlockEntity extends BasinBlockEntity {
     @Override
     public void addBehaviours(List<BlockEntityBehaviour> behaviours) {
         super.addBehaviours(behaviours);
+    }
+
+    @Override
+    public boolean addToGoggleTooltip(List<Component> tooltip, boolean isPlayerSneaking) {
+        LANG.translate("gui.goggles.castingbasin_contents")
+                .forGoggles(tooltip);
+
+        IItemHandlerModifiable items = itemCapability.orElse(new ItemStackHandler());
+        IFluidHandler fluids = fluidCapability.orElse(new FluidTank(0));
+        boolean isEmpty = true;
+
+        for (int i = 0; i < items.getSlots(); i++) {
+            ItemStack stackInSlot = items.getStackInSlot(i);
+            if (stackInSlot.isEmpty())
+                continue;
+            LANG.text("")
+                    .add(Components.translatable(stackInSlot.getDescriptionId())
+                            .withStyle(ChatFormatting.GRAY))
+                    .add(LANG.text(" x" + stackInSlot.getCount())
+                            .style(ChatFormatting.GREEN))
+                    .forGoggles(tooltip, 1);
+            isEmpty = false;
+        }
+
+        LangBuilder mb = LANG.translate("generic.unit.millibuckets");
+        for (int i = 0; i < fluids.getTanks(); i++) {
+            FluidStack fluidStack = fluids.getFluidInTank(i);
+            if (fluidStack.isEmpty())
+                continue;
+            LANG.text("")
+                    .add(LANG.fluidName(fluidStack)
+                            .add(LANG.text(" "))
+                            .style(ChatFormatting.GRAY)
+                            .add(LANG.number(fluidStack.getAmount())
+                                    .add(mb)
+                                    .style(ChatFormatting.BLUE)))
+                    .forGoggles(tooltip, 1);
+            isEmpty = false;
+        }
+
+        if (isEmpty)
+            tooltip.remove(0);
+
+        return true;
     }
 }
