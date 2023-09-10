fcb9cdf797fe0e65cac2515a3af458ca730ac517
Luc_Creeper74 <lucreeper74@gmail.com>
2023-09-10 23:08:27 +0200

    Fix for tossed items

--- src/main/java/fr/lucreeper74/createmetallurgy/content/castingbasin/CastingBasinBlock.java
+++ src/main/java/fr/lucreeper74/createmetallurgy/content/castingbasin/CastingBasinBlock.java
@@ -2,17 +2,44 @@ package fr.lucreeper74.createmetallurgy.content.castingbasin;
 
 import com.simibubi.create.content.equipment.wrench.IWrenchable;
 import com.simibubi.create.content.processing.basin.BasinBlock;
-import com.simibubi.create.content.processing.basin.BasinBlockEntity;
-import com.simibubi.create.foundation.block.IBE;
 import fr.lucreeper74.createmetallurgy.registries.AllBlockEntityTypes;
+import fr.lucreeper74.createmetallurgy.registries.AllBlocks;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.items.ItemHandlerHelper;
 
-public class CastingBasinBlock extends BasinBlock implements IBE<BasinBlockEntity>, IWrenchable {
+public class CastingBasinBlock extends BasinBlock implements IWrenchable {
     public CastingBasinBlock(Properties p_i48440_1_) {
         super(p_i48440_1_);
     }
     @Override
     public BlockEntityType<? extends CastingBasinBlockEntity> getBlockEntityType() {
         return AllBlockEntityTypes.CASTING_BASIN.get();
+    }
+
+    @Override
+    public void updateEntityAfterFallOn(BlockGetter worldIn, Entity entityIn) {
+        super.updateEntityAfterFallOn(worldIn, entityIn);
+        if (!AllBlocks.CASTING_BASIN_BLOCK.has(worldIn.getBlockState(entityIn.blockPosition())))
+            return;
+        if (!(entityIn instanceof ItemEntity itemEntity))
+            return;
+        if (!entityIn.isAlive())
+            return;
+        withBlockEntityDo(worldIn, entityIn.blockPosition(), be -> {
+            CastingBasinBlockEntity cbe = (CastingBasinBlockEntity) be;
+            ItemStack insertItem = ItemHandlerHelper.insertItem(cbe.inputInventory, itemEntity.getItem()
+                    .copy(), false);
+
+            if (insertItem.isEmpty()) {
+                itemEntity.discard();
+                return;
+            }
+
+            itemEntity.setItem(insertItem);
+        });
     }
 }
