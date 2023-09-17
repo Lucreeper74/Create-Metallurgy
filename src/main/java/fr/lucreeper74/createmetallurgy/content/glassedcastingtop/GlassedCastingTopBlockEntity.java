efbc77e30b83b12029aa21779644a9fcd7e95f6a
Luc_Creeper74 <lucreeper74@gmail.com>
2023-09-17 23:42:58 +0200

    Added FoundryTopGlassed version

---
+++ src/main/java/fr/lucreeper74/createmetallurgy/content/glassedcastingtop/GlassedCastingTopBlockEntity.java
@@ -0,0 +1,12 @@
+package fr.lucreeper74.createmetallurgy.content.glassedcastingtop;
+
+import fr.lucreeper74.createmetallurgy.content.castingtop.CastingTopBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class GlassedCastingTopBlockEntity extends CastingTopBlockEntity {
+    public GlassedCastingTopBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+}
