945a098dadbc84639422eb3c72b454f18b3ab117
Luc_Creeper74 <lucreeper74@gmail.com>
2023-09-25 11:20:13 +0200

    Re-packaged Melting recipe

---
+++ src/main/java/fr/lucreeper74/createmetallurgy/content/processing/castingtop/MeltingRecipe.java
@@ -0,0 +1,11 @@
+package fr.lucreeper74.createmetallurgy.content.processing.castingtop;
+
+import com.simibubi.create.content.processing.basin.BasinRecipe;
+import com.simibubi.create.content.processing.recipe.ProcessingRecipeBuilder;
+import fr.lucreeper74.createmetallurgy.registries.AllRecipeTypes;
+
+public class MeltingRecipe extends BasinRecipe {
+    public MeltingRecipe(ProcessingRecipeBuilder.ProcessingRecipeParams params) {
+            super(AllRecipeTypes.MELTING, params);
+        }
+}
