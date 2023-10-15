91b2291f0b570d34edce4572b7c68fe8839590ca
Luc_Creeper74 <lucreeper74@gmail.com>
2023-10-15 13:51:10 +0200

    Fix fluids tags

--- src/main/java/fr/lucreeper74/createmetallurgy/registries/AllFluids.java
+++ src/main/java/fr/lucreeper74/createmetallurgy/registries/AllFluids.java
@@ -3,6 +3,9 @@ package fr.lucreeper74.createmetallurgy.registries;
 import com.tterrag.registrate.util.entry.FluidEntry;
 import fr.lucreeper74.createmetallurgy.CreateMetallurgy;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.FluidTags;
+import net.minecraftforge.common.SoundActions;
 import net.minecraftforge.fluids.ForgeFlowingFluid;
 
 import static fr.lucreeper74.createmetallurgy.CreateMetallurgy.REGISTRATE;
@@ -13,14 +16,17 @@ public class AllFluids {
     // Molten Iron
     public static final ResourceLocation MOLTEN_IRON_STILL_RL = CreateMetallurgy.genRL("fluid/molten_iron_still");
     public static final ResourceLocation MOLTEN_IRON_FLOW_RL = CreateMetallurgy.genRL("fluid/molten_iron_flow");
-
     public static final FluidEntry<ForgeFlowingFluid.Flowing> MOLTEN_IRON = REGISTRATE.fluid("molten_iron", MOLTEN_IRON_STILL_RL, MOLTEN_IRON_FLOW_RL)
             .properties(b -> b.viscosity(2000)
-                    .density(1400))
+                    .density(1400)
+                    .lightLevel(10)
+                    .sound(SoundActions.BUCKET_EMPTY, SoundEvents.BUCKET_EMPTY_LAVA)
+                    .sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL_LAVA))
             .fluidProperties(p -> p.levelDecreasePerBlock(2)
                     .tickRate(25)
                     .slopeFindDistance(3)
                     .explosionResistance(100f))
+            .tag(FluidTags.LAVA)
             .source(ForgeFlowingFluid.Source::new)
             .bucket()
             .build()
@@ -31,11 +37,15 @@ public class AllFluids {
     public static final ResourceLocation MOLTEN_GOLD_FLOW_RL = CreateMetallurgy.genRL("fluid/molten_gold_flow");
     public static final FluidEntry<ForgeFlowingFluid.Flowing> MOLTEN_GOLD = REGISTRATE.fluid("molten_gold", MOLTEN_GOLD_STILL_RL, MOLTEN_GOLD_FLOW_RL)
             .properties(b -> b.viscosity(2000)
-                    .density(1400))
+                    .density(1400)
+                    .lightLevel(10)
+                    .sound(SoundActions.BUCKET_EMPTY, SoundEvents.BUCKET_EMPTY_LAVA)
+                    .sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL_LAVA))
             .fluidProperties(p -> p.levelDecreasePerBlock(2)
                     .tickRate(25)
                     .slopeFindDistance(3)
                     .explosionResistance(100f))
+            .tag(FluidTags.LAVA)
             .source(ForgeFlowingFluid.Source::new)
             .bucket()
             .build()
@@ -46,11 +56,15 @@ public class AllFluids {
     public static final ResourceLocation MOLTEN_COPPER_FLOW_RL = CreateMetallurgy.genRL("fluid/molten_copper_flow");
     public static final FluidEntry<ForgeFlowingFluid.Flowing> MOLTEN_COPPER = REGISTRATE.fluid("molten_copper", MOLTEN_COPPER_STILL_RL, MOLTEN_COPPER_FLOW_RL)
             .properties(b -> b.viscosity(2000)
-                    .density(1400))
+                    .density(1400)
+                    .lightLevel(10)
+                    .sound(SoundActions.BUCKET_EMPTY, SoundEvents.BUCKET_EMPTY_LAVA)
+                    .sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL_LAVA))
             .fluidProperties(p -> p.levelDecreasePerBlock(2)
                     .tickRate(25)
                     .slopeFindDistance(3)
                     .explosionResistance(100f))
+            .tag(FluidTags.LAVA)
             .source(ForgeFlowingFluid.Source::new)
             .bucket()
             .build()
@@ -61,11 +75,15 @@ public class AllFluids {
     public static final ResourceLocation MOLTEN_ZINC_FLOW_RL = CreateMetallurgy.genRL("fluid/molten_zinc_flow");
     public static final FluidEntry<ForgeFlowingFluid.Flowing> MOLTEN_ZINC = REGISTRATE.fluid("molten_zinc", MOLTEN_ZINC_STILL_RL, MOLTEN_ZINC_FLOW_RL)
             .properties(b -> b.viscosity(2000)
-                    .density(1400))
+                    .density(1400)
+                    .lightLevel(10)
+                    .sound(SoundActions.BUCKET_EMPTY, SoundEvents.BUCKET_EMPTY_LAVA)
+                    .sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL_LAVA))
             .fluidProperties(p -> p.levelDecreasePerBlock(2)
                     .tickRate(25)
                     .slopeFindDistance(3)
                     .explosionResistance(100f))
+            .tag(FluidTags.LAVA)
             .source(ForgeFlowingFluid.Source::new)
             .bucket()
             .build()
@@ -76,11 +94,15 @@ public class AllFluids {
     public static final ResourceLocation MOLTEN_BRASS_FLOW_RL = CreateMetallurgy.genRL("fluid/molten_brass_flow");
     public static final FluidEntry<ForgeFlowingFluid.Flowing> MOLTEN_BRASS = REGISTRATE.fluid("molten_brass", MOLTEN_BRASS_STILL_RL, MOLTEN_BRASS_FLOW_RL)
             .properties(b -> b.viscosity(2000)
-                    .density(1400))
+                    .density(1400)
+                    .lightLevel(10)
+                    .sound(SoundActions.BUCKET_EMPTY, SoundEvents.BUCKET_EMPTY_LAVA)
+                    .sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL_LAVA))
             .fluidProperties(p -> p.levelDecreasePerBlock(2)
                     .tickRate(25)
                     .slopeFindDistance(3)
                     .explosionResistance(100f))
+            .tag(FluidTags.LAVA)
             .source(ForgeFlowingFluid.Source::new)
             .bucket()
             .build()
