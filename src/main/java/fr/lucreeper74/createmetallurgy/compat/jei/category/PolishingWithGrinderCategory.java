54638c9c83bb372fb00b7008688c7b6b57ea0e7d
Luc_Creeper74 <lucreeper74@gmail.com>
2024-04-07 19:42:02 +0200

    Added Mechanical Belt Grinder

---
+++ src/main/java/fr/lucreeper74/createmetallurgy/compat/jei/category/PolishingWithGrinderCategory.java
@@ -0,0 +1,51 @@
+package fr.lucreeper74.createmetallurgy.compat.jei.category;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.simibubi.create.compat.jei.category.CreateRecipeCategory;
+import com.simibubi.create.content.equipment.sandPaper.SandPaperPolishingRecipe;
+import com.simibubi.create.content.processing.recipe.ProcessingOutput;
+import com.simibubi.create.foundation.gui.AllGuiTextures;
+import fr.lucreeper74.createmetallurgy.compat.jei.category.animations.AnimatedBeltGrinder;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+
+import java.util.List;
+
+public class PolishingWithGrinderCategory extends CreateRecipeCategory<SandPaperPolishingRecipe> {
+    private final AnimatedBeltGrinder beltGrinder = new AnimatedBeltGrinder();
+
+    public PolishingWithGrinderCategory(Info<SandPaperPolishingRecipe> info) {
+        super(info);
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, SandPaperPolishingRecipe recipe, IFocusGroup focuses) {
+        builder
+                .addSlot(RecipeIngredientRole.INPUT, 44, 5)
+                .setBackground(getRenderedSlot(), -1, -1)
+                .addIngredients(recipe.getIngredients().get(0));
+
+        List<ProcessingOutput> results = recipe.getRollableResults();
+        int i = 0;
+        for (ProcessingOutput output : results) {
+            int xOffset = i % 2 == 0 ? 0 : 19;
+            int yOffset = (i / 2) * -19;
+            builder
+                    .addSlot(RecipeIngredientRole.OUTPUT, 118 + xOffset, 48 + yOffset)
+                    .setBackground(getRenderedSlot(output), -1, -1)
+                    .addItemStack(output.getStack())
+                    .addTooltipCallback(addStochasticTooltip(output));
+            i++;
+        }
+    }
+
+    @Override
+    public void draw(SandPaperPolishingRecipe recipe, IRecipeSlotsView iRecipeSlotsView, PoseStack matrixStack, double mouseX, double mouseY) {
+        AllGuiTextures.JEI_DOWN_ARROW.render(matrixStack, 70, 6);
+        AllGuiTextures.JEI_SHADOW.render(matrixStack, 72 - 17, 42 + 13);
+
+        beltGrinder.draw(matrixStack, 72, 42);
+    }
+}
