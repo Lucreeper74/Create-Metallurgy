53fb21835878939a82679a778a4cb951f16c50a3
Luc_Creeper74 <lucreeper74@gmail.com>
2024-04-13 22:37:28 +0200

    Added Belt Grinder filter slot

--- src/main/java/fr/lucreeper74/createmetallurgy/content/kinetics/beltGrinder/BeltGrinderFilterSlot.java
+++ src/main/java/fr/lucreeper74/createmetallurgy/content/kinetics/beltGrinder/BeltGrinderFilterSlot.java
@@ -3,8 +3,10 @@ package fr.lucreeper74.createmetallurgy.content.kinetics.beltGrinder;
 import com.jozufozu.flywheel.util.transform.TransformStack;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.foundation.blockEntity.behaviour.ValueBoxTransform;
+import com.simibubi.create.foundation.utility.AngleHelper;
 import com.simibubi.create.foundation.utility.VecHelper;
 
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 
@@ -12,16 +14,17 @@ public class BeltGrinderFilterSlot extends ValueBoxTransform {
 
     @Override
     public Vec3 getLocalOffset(BlockState state) {
-        int offset = -3;
-        Vec3 x = VecHelper.voxelSpace(8, 12.5f, 8 + offset);
-        Vec3 z = VecHelper.voxelSpace(8 + offset, 12.5f, 8);
-        return z;
+        return VecHelper.rotateCentered(VecHelper.voxelSpace(8, 12.5, 5), angleY(state), Direction.Axis.Y);
     }
 
     @Override
     public void rotate(BlockState state, PoseStack ms) {
         TransformStack.cast(ms)
+                .rotateY(angleY(state))
                 .rotateX(90);
     }
 
+    protected float angleY(BlockState state) {
+        return AngleHelper.horizontalAngle(state.getValue(BeltGrinderBlock.HORIZONTAL_FACING).getOpposite());
+    }
 }
