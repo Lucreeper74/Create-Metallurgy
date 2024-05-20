74058d68fe3198e7b49bfa8760c57643ebc9f43e
Luc_Creeper74 <lucreeper74@gmail.com>
2024-05-20 19:52:06 +0200

    Added Light Bulbs ponder scenes

---
+++ src/main/java/fr/lucreeper74/createmetallurgy/ponders/LightBulbScenes.java
@@ -0,0 +1,109 @@
+package fr.lucreeper74.createmetallurgy.ponders;
+
+import com.simibubi.create.content.redstone.analogLever.AnalogLeverBlockEntity;
+import com.simibubi.create.content.redstone.link.RedstoneLinkBlockEntity;
+import com.simibubi.create.foundation.ponder.PonderPalette;
+import com.simibubi.create.foundation.ponder.SceneBuilder;
+import com.simibubi.create.foundation.ponder.SceneBuildingUtil;
+import com.simibubi.create.foundation.ponder.Selection;
+import com.simibubi.create.foundation.ponder.element.InputWindowElement;
+import com.simibubi.create.foundation.utility.Pointing;
+import fr.lucreeper74.createmetallurgy.content.redstone.lightbulb.LightBulbBlock;
+import fr.lucreeper74.createmetallurgy.content.redstone.lightbulb.LightBulbBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.RedStoneWireBlock;
+import net.minecraft.world.phys.Vec3;
+
+public class LightBulbScenes {
+    public static void lightBulbScenes(SceneBuilder scene, SceneBuildingUtil util) {
+        scene.title("light_bulbs", "Using Light Bulbs");
+        scene.configureBasePlate(0, 0, 5);
+        scene.showBasePlate();
+        scene.idle(10);
+
+        BlockPos whiteBulbPos = util.grid.at(1, 1, 1);
+        BlockPos redBulbPos = util.grid.at(4, 2, 3);
+        BlockPos greenBulbPos = util.grid.at(2, 2, 3);
+        BlockPos BrownBulbPos = util.grid.at(0, 2, 3);
+        BlockPos leverPos = util.grid.at(3, 1, 1);
+        BlockPos redstonePos = leverPos.west();
+
+        Selection whiteBulb = util.select.position(whiteBulbPos);
+        Selection redBulb = util.select.position(redBulbPos);
+        Selection greenBulb = util.select.position(greenBulbPos);
+        Selection brownBulb = util.select.position(BrownBulbPos);
+
+        Vec3 whiteBulbVec = util.vector.blockSurface(whiteBulbPos, Direction.DOWN)
+                .add(0, 12 / 16f, 0);
+        Vec3 greenBulbVec = util.vector.blockSurface(greenBulbPos, Direction.DOWN)
+                .add(0, 12 / 16f, 0);
+        Vec3 leverVec = util.vector.centerOf(leverPos)
+                .add(0, -.25, 0);
+
+        //Intro
+        scene.world.showSection(util.select.position(leverPos), Direction.DOWN);
+        scene.idle(5);
+        scene.world.showSection(util.select.position(redstonePos), Direction.DOWN);
+        scene.idle(5);
+        scene.world.showSection(whiteBulb, Direction.DOWN);
+        scene.idle(10);
+
+        scene.overlay.showText(80)
+                .attachKeyFrame()
+                .text("Light Bulbs are an alternative light source to the Redstone Lamp")
+                .pointAt(whiteBulbVec)
+                .placeNearTarget();
+        scene.idle(90);
+
+        //Turn on first bulb
+        scene.overlay.showText(80)
+                .attachKeyFrame()
+                .text("They emit light proportionally to the strength of the redstone signal received")
+                .pointAt(whiteBulbVec)
+                .placeNearTarget();
+        scene.overlay.showControls(new InputWindowElement(leverVec, Pointing.DOWN).rightClick(), 40);
+        scene.idle(10);
+        for (int i = 0; i < 8; i++) {
+            scene.idle(5);
+            final int state = i + 1;
+            scene.world.modifyBlockEntityNBT(util.select.position(leverPos), AnalogLeverBlockEntity.class,
+                    nbt -> nbt.putInt("State", state));
+            scene.world.modifyBlock(redstonePos, s -> s.setValue(RedStoneWireBlock.POWER, state), false);
+            scene.effects.indicateRedstone(redstonePos);
+            scene.world.modifyBlock(whiteBulbPos, s -> s.setValue(LightBulbBlock.LEVEL, state), false);
+        }
+        scene.idle(40);
+
+        //Add others bulbs
+        scene.world.showSection(util.select.fromTo(4, 1, 3, 0, 1, 3), Direction.NORTH);
+        scene.idle(5);
+        scene.world.showSection(redBulb, Direction.DOWN);
+        scene.idle(5);
+        scene.world.showSection(greenBulb, Direction.DOWN);
+        scene.idle(5);
+        scene.world.showSection(brownBulb, Direction.DOWN);
+        scene.idle(10);
+
+        scene.overlay.showOutline(PonderPalette.WHITE, new Object(), redBulb, 80);
+        scene.overlay.showOutline(PonderPalette.WHITE, new Object(), greenBulb, 80);
+        scene.overlay.showOutline(PonderPalette.WHITE, new Object(), brownBulb, 80);
+        scene.overlay.showText(80)
+                .attachKeyFrame()
+                .text("Light Bulbs are available in all colors")
+                .pointAt(greenBulbVec)
+                .placeNearTarget();
+        scene.idle(90);
+
+        scene.overlay.showFilterSlotInput(whiteBulbVec, Direction.UP, 100);
+        scene.overlay.showText(80)
+                .attachKeyFrame()
+                .text("Placing items in Address slot can connect others light bulbs in a Network")
+                .pointAt(whiteBulbVec)
+                .placeNearTarget();
+        scene.idle(90);
+    }
+}
--- src/main/java/fr/lucreeper74/createmetallurgy/registries/CMPonders.java
+++ src/main/java/fr/lucreeper74/createmetallurgy/registries/CMPonders.java
@@ -5,6 +5,7 @@ import com.simibubi.create.foundation.ponder.PonderRegistrationHelper;
 import fr.lucreeper74.createmetallurgy.CreateMetallurgy;
 import fr.lucreeper74.createmetallurgy.ponders.CastingScenes;
 import fr.lucreeper74.createmetallurgy.ponders.FoundryScenes;
+import fr.lucreeper74.createmetallurgy.ponders.LightBulbScenes;
 
 public class CMPonders {
     static final PonderRegistrationHelper HELPER = new PonderRegistrationHelper(CreateMetallurgy.MOD_ID);
@@ -14,13 +15,19 @@ public class CMPonders {
         // (!) Added entries require re-launch
         // (!) Modifications inside storyboard methods only require re-opening the ui
 
-        HELPER.forComponents(CMBlocks.FOUNDRY_BASIN_BLOCK).addStoryBoard("foundry_basin", FoundryScenes::foundryBasin)
+        HELPER.forComponents(CMBlocks.FOUNDRY_BASIN_BLOCK)
+                .addStoryBoard("foundry_basin", FoundryScenes::foundryBasin)
                 .addStoryBoard("foundry_mixer", FoundryScenes::alloying);
 
-        HELPER.forComponents(CMBlocks.FOUNDRY_MIXER_BLOCK).addStoryBoard("foundry_mixer", FoundryScenes::alloying);
+        HELPER.forComponents(CMBlocks.FOUNDRY_MIXER_BLOCK)
+                .addStoryBoard("foundry_mixer", FoundryScenes::alloying);
 
 
-        HELPER.forComponents(CMBlocks.CASTING_BASIN_BLOCK, CMBlocks.CASTING_TABLE_BLOCK).addStoryBoard("casting_blocks", CastingScenes::castingBlocks);
+        HELPER.forComponents(CMBlocks.CASTING_BASIN_BLOCK, CMBlocks.CASTING_TABLE_BLOCK)
+                .addStoryBoard("casting_blocks", CastingScenes::castingBlocks);
+
+        HELPER.forComponents(CMBlocks.LIGHT_BULBS.toArray())
+                .addStoryBoard("light_bulbs", LightBulbScenes::lightBulbScenes);
     }
 
     public static void registerLang() {
