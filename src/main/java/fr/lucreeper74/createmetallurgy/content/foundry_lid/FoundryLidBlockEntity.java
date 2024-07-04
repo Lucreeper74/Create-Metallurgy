1faec3ed4dadf2f77de9bc67cdecc6358b7f601d
Luc_Creeper74 <lucreeper74@gmail.com>
2024-07-04 19:44:04 +0200

    Fix crash when use Create:LowHeated

--- src/main/java/fr/lucreeper74/createmetallurgy/content/foundry_lid/FoundryLidBlockEntity.java
+++ src/main/java/fr/lucreeper74/createmetallurgy/content/foundry_lid/FoundryLidBlockEntity.java
@@ -1,4 +1,4 @@
-package fr.lucreeper74.createmetallurgy.content.processing.foundrylid;
+package fr.lucreeper74.createmetallurgy.content.foundry_lid;
 
 import com.simibubi.create.content.processing.basin.BasinBlockEntity;
 import com.simibubi.create.content.processing.basin.BasinOperatingBlockEntity;
@@ -6,7 +6,7 @@ import com.simibubi.create.content.processing.burner.BlazeBurnerBlock;
 import com.simibubi.create.content.processing.recipe.ProcessingRecipe;
 import com.simibubi.create.foundation.utility.VecHelper;
 import com.simibubi.create.foundation.utility.animation.LerpedFloat;
-import fr.lucreeper74.createmetallurgy.content.processing.foundrybasin.FoundryBasinBlockEntity;
+import fr.lucreeper74.createmetallurgy.content.foundry_basin.FoundryBasinBlockEntity;
 import fr.lucreeper74.createmetallurgy.registries.CMRecipeTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
@@ -25,7 +25,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
-import static fr.lucreeper74.createmetallurgy.content.processing.foundrylid.FoundryLidBlock.OPEN;
+import static fr.lucreeper74.createmetallurgy.content.foundry_lid.FoundryLidBlock.OPEN;
 
 public class FoundryLidBlockEntity extends BasinOperatingBlockEntity {
 
@@ -78,7 +78,7 @@ public class FoundryLidBlockEntity extends BasinOperatingBlockEntity {
 
         if(level.isClientSide) {
             gauge.tickChaser();
-            gauge.chase((double) temp.get(BasinBlockEntity.getHeatLevelOf(this.getLevel().getBlockState(getBlockPos().below(2)))) / 2000, .2f, LerpedFloat.Chaser.EXP);
+            gauge.chase((double) temp.getOrDefault(BasinBlockEntity.getHeatLevelOf(this.getLevel().getBlockState(getBlockPos().below(2))), 0) / 2000, .2f, LerpedFloat.Chaser.EXP);
         }
 
         if (!level.isClientSide && (currentRecipe == null || processingTime == -1)) {
