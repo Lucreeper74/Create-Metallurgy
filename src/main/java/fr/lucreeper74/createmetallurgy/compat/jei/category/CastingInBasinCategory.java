5a1cc158df5f4ccb2822159bac34ec8f461422c4
Luc_Creeper74 <lucreeper74@gmail.com>
2024-07-18 13:26:38 +0200

    Added mold condition on JEI for casting recipes

--- src/main/java/fr/lucreeper74/createmetallurgy/compat/jei/category/CastingInBasinCategory.java
+++ src/main/java/fr/lucreeper74/createmetallurgy/compat/jei/category/CastingInBasinCategory.java
@@ -4,9 +4,11 @@ import com.simibubi.create.compat.jei.category.CreateRecipeCategory;
 import com.simibubi.create.content.processing.recipe.ProcessingOutput;
 import com.simibubi.create.foundation.fluid.FluidIngredient;
 import com.simibubi.create.foundation.gui.AllGuiTextures;
+import com.simibubi.create.foundation.gui.AllIcons;
 import com.simibubi.create.foundation.utility.Components;
 import fr.lucreeper74.createmetallurgy.compat.jei.category.elements.CastingInBasinElement;
 import fr.lucreeper74.createmetallurgy.content.casting.recipe.CastingBasinRecipe;
+import fr.lucreeper74.createmetallurgy.utils.CMLang;
 import mezz.jei.api.forge.ForgeTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
@@ -15,7 +17,11 @@ import mezz.jei.api.recipe.RecipeIngredientRole;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CastingInBasinCategory extends CreateRecipeCategory<CastingBasinRecipe> {
@@ -34,10 +40,13 @@ public class CastingInBasinCategory extends CreateRecipeCategory<CastingBasinRec
                 .setBackground(getRenderedSlot(), -1, -1)
                 .addIngredients(ForgeTypes.FLUID_STACK, withImprovedVisibility(fluidIngredient.getMatchingFluidStacks()))
                 .addTooltipCallback(addFluidTooltip(fluidIngredient.getRequiredAmount()));
-        builder
-                .addSlot(RecipeIngredientRole.INPUT, 15, 26)
-                .setBackground(getRenderedSlot(), -1, -1)
-                .addIngredients(recipe.getIngredient());
+
+        Ingredient mold = recipe.getIngredient();
+        if (!mold.isEmpty())
+            builder
+                    .addSlot(RecipeIngredientRole.INPUT, 15, 26)
+                    .setBackground(getRenderedSlot(), -1, -1)
+                    .addIngredients(mold);
 
         ProcessingOutput output = recipe.getProcessingOutput();
         builder
@@ -52,9 +61,33 @@ public class CastingInBasinCategory extends CreateRecipeCategory<CastingBasinRec
                      double mouseX, double mouseY) {
         AllGuiTextures.JEI_ARROW.render(graphics, 85, 32);
         AllGuiTextures.JEI_DOWN_ARROW.render(graphics, 43, 4);
+
+        if (!recipe.getIngredient().isEmpty() && recipe.isMoldConsumed())
+            AllIcons.I_DISABLE.render(graphics, 14, 44);
+
         castingBasin.draw(graphics, 48, 27);
 
         graphics.drawString(Minecraft.getInstance().font, Components.translatable(((float) recipe.getProcessingDuration() / 20.0F) + "s").withStyle(ChatFormatting.GRAY),
                 95, 26, 0xffffff);
+    }
+
+    @Override
+    @NotNull
+    public List<Component> getTooltipStrings(CastingBasinRecipe recipe, IRecipeSlotsView recipeSlotsView, double mouseX, double mouseY) {
+        List<Component> tooltip = new ArrayList<>();
+
+        if (!recipe.getIngredient().isEmpty() && recipe.isMoldConsumed()) {
+            int minX = 14;
+            int maxX = minX + 18;
+            int minY = 44;
+            int maxY = minY + 18;
+
+            if (mouseX >= minX && mouseX < maxX && mouseY >= minY && mouseY < maxY) {
+                tooltip.add(CMLang.translateDirect("recipe.casting.mold_consumed").withStyle(ChatFormatting.RED));
+                return tooltip;
+            }
+        }
+
+        return tooltip;
     }
 }
