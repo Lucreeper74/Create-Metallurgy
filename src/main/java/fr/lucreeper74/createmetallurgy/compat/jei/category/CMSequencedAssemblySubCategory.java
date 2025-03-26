7d66a8ee161e20f4b3c573a104cade3760410cba
Luc_Creeper74 <lucreeper74@gmail.com>
2025-03-26 19:13:44 +0100

    Grinding recipe now compatible with Sequenced Assembly

---
+++ src/main/java/fr/lucreeper74/createmetallurgy/compat/jei/category/CMSequencedAssemblySubCategory.java
@@ -0,0 +1,31 @@
+package fr.lucreeper74.createmetallurgy.compat.jei.category;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.simibubi.create.compat.jei.category.sequencedAssembly.SequencedAssemblySubCategory;
+import com.simibubi.create.content.processing.sequenced.SequencedRecipe;
+import fr.lucreeper74.createmetallurgy.compat.jei.category.animations.AnimatedBeltGrinder;
+
+public abstract class CMSequencedAssemblySubCategory extends SequencedAssemblySubCategory {
+    public CMSequencedAssemblySubCategory(int width) {
+        super(width);
+    }
+
+    public static class AssemblyGrinding extends CMSequencedAssemblySubCategory {
+
+        AnimatedBeltGrinder grinder;
+
+        public AssemblyGrinding() {
+            super(25);
+            grinder = new AnimatedBeltGrinder();
+        }
+
+        @Override
+        public void draw(SequencedRecipe<?> recipe, PoseStack ms, double mouseX, double mouseY, int index) {
+            ms.pushPose();
+            ms.translate(0, 51.5f, 0);
+            ms.scale(.6f, .6f, .6f);
+            grinder.draw(ms, getWidth() / 2, 30);
+            ms.popPose();
+        }
+    }
+}
---
+++ src/main/java/fr/lucreeper74/createmetallurgy/content/blocks/belt_grinder/GrindingRecipe.java
@@ -0,0 +1,70 @@
+package fr.lucreeper74.createmetallurgy.content.blocks.belt_grinder;
+
+import com.simibubi.create.compat.jei.category.sequencedAssembly.SequencedAssemblySubCategory;
+import com.simibubi.create.content.processing.recipe.ProcessingRecipe;
+import com.simibubi.create.content.processing.recipe.ProcessingRecipeBuilder;
+import com.simibubi.create.content.processing.sequenced.IAssemblyRecipe;
+import fr.lucreeper74.createmetallurgy.compat.jei.category.CMSequencedAssemblySubCategory;
+import fr.lucreeper74.createmetallurgy.registries.CMBlocks;
+import fr.lucreeper74.createmetallurgy.registries.CMRecipeTypes;
+import fr.lucreeper74.createmetallurgy.utils.CMLang;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.items.wrapper.RecipeWrapper;
+
+import java.util.List;
+import java.util.Set;
+import java.util.function.Supplier;
+
+public class GrindingRecipe extends ProcessingRecipe<RecipeWrapper> implements IAssemblyRecipe {
+
+	public GrindingRecipe(ProcessingRecipeBuilder.ProcessingRecipeParams params) {
+        super(CMRecipeTypes.GRINDING, params);
+    }
+
+    @Override
+    public boolean matches(RecipeWrapper inv, Level worldIn) {
+        if (inv.isEmpty())
+            return false;
+        return ingredients.get(0)
+                .test(inv.getItem(0));
+    }
+
+    @Override
+    protected int getMaxInputCount() {
+        return 1;
+    }
+
+    @Override
+    protected int getMaxOutputCount() {
+        return 4;
+    }
+
+    @Override
+    protected boolean canSpecifyDuration() {
+        return true;
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public Component getDescriptionForAssembly() {
+        return CMLang.translateDirect("recipe.assembly.grinding");
+    }
+
+    @Override
+    public void addRequiredMachines(Set<ItemLike> list) {
+        list.add(CMBlocks.BELT_GRINDER_BLOCK.get());
+    }
+
+    @Override
+    public void addAssemblyIngredients(List<Ingredient> list) {}
+
+    @Override
+    public Supplier<Supplier<SequencedAssemblySubCategory>> getJEISubCategory() {
+        return () -> CMSequencedAssemblySubCategory.AssemblyGrinding::new;
+    }
+}
