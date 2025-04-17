9598e4af4103ae48bab026b26d21d436a5abfe97
Luc_Creeper74 <lucreeper74@gmail.com>
2025-04-17 22:45:55 +0200

    Fixed Entity Melting recipes

--- src/main/java/fr/lucreeper74/createmetallurgy/content/blocks/industrial_crucible/foundry/recipes/FoundryRecipe.java
+++ src/main/java/fr/lucreeper74/createmetallurgy/content/blocks/industrial_crucible/foundry/recipes/FoundryRecipe.java
@@ -91,8 +91,8 @@ public class FoundryRecipe extends ProcessingRecipe<SmartInventory> {
                     // No matching fluid
                     return false;
                 }
-                return true;
             }
+            return true;
         }
         return false;
     }
