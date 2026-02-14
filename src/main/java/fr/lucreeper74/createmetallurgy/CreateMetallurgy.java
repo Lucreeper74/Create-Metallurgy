f3cbbe10336d8c25e673579c55da21ffc4777dec
Lucreeper74 <lucreeper74@gmail.com>
2026-02-14 16:16:44 +0100

    Clean-up imports

--- src/main/java/fr/lucreeper74/createmetallurgy/CreateMetallurgy.java
+++ src/main/java/fr/lucreeper74/createmetallurgy/CreateMetallurgy.java
@@ -1,7 +1,6 @@
 package fr.lucreeper74.createmetallurgy;
 
 import com.mojang.logging.LogUtils;
-import com.simibubi.create.AllFluids;
 import com.simibubi.create.foundation.CreateNBTProcessors;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.simibubi.create.foundation.item.ItemDescription;
